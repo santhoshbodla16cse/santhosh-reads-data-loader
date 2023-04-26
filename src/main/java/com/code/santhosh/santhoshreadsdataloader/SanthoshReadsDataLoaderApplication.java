f42e3f80884e0d004912aa68bfa52a11eba76f93
Santhosh Bodla <santhosh.bodla.16cse>
2023-04-25 23:08:07 -0700

    "Initial commit"

---
+++ src/main/java/com/code/santhosh/santhoshreadsdataloader/SanthoshReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.code.santhosh.santhoshreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanthoshReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanthoshReadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/santhosh/santhoshreadsdataloader/SanthoshReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.santhosh.santhoshreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanthoshReadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
